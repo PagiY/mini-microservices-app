c889869831d6035df163041522f51c3a2b96ff87
Paula Yap <pagiyap003@gmail.com>
2025-04-23 06:04:08 +0800

    created posts-service with the 2 endpoints: GET posts POST post

---
+++ posts-service/app/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(express.json());
+
+// save responses in memory
+const posts = {};
+
+app.get('/post', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/post', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = {
+    id, title
+  }
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => console.log('Listening on PORT 4000'))
